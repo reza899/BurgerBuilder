528fd2a467000c2acf912786a90eee989b5e80d1
Reza <reza.bozorgi@outlook.com>
2020-10-02 16:32:26 +0330

    15. Adding the Build Control Component

--- src/components/Layout/Burger/Burger.js
+++ src/components/Layout/Burger/Burger.js
@@ -2,16 +2,21 @@ import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-function Burger() {
+const Burger = (props) => {
+  const ingredientTrasformation = Object.keys(props.ingredients).map(
+    (igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={i + igKey} type={igKey} />;
+      });
+    }
+  );
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="chees" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="meat" />
+      {ingredientTrasformation}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
-}
+};
 
 export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,47 @@ import React, { Component } from "react";
 import Burger from "../../components/Layout/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      meat: 1,
+      bacon: 2,
+      cheese: 2,
+      salad: 2,
+    },
+    ingredientPrices: {
+      meat: 20,
+      bacon: 18,
+      cheese: 5,
+      salad: 2,
+    },
+  };
+
+  changeIgHandler = (e, type) => {
+    let ig = { ...this.state.ingredients };
+    ig[type] = e.target.value;
+
+    this.setState({ ingredients: ig });
+  };
   render() {
     return (
       <>
-        <Burger />
-        <div>Burger Builder</div>
+        <Burger ingredients={this.state.ingredients} />
+        <div>
+          Meat:{" "}
+          <input
+            type="number"
+            min="0"
+            value={this.state.ingredients.meat}
+            onChange={(e) => this.changeIgHandler(e, "meat")}
+          />
+          Bacon:{" "}
+          <input
+            type="number"
+            min="0"
+            value={this.state.ingredients.bacon}
+            onChange={(e) => this.changeIgHandler(e, "bacon")}
+          />
+        </div>
       </>
     );
   }
