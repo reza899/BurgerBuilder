a9e5f774c186b92fd1973ee255988539b3fe1d1a
Reza <reza.bozorgi@outlook.com>
2020-10-04 19:21:47 +0330

    31. Working on Responsive Adjustments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,10 +2,10 @@ import React from "react";
 import BurgerLogo from "../../assets/images/burger-logo.jpg";
 import classes from "./Logo.module.css";
 
-const Logo = () => {
+const Logo = (props) => {
   return (
     <div className={classes.Logo}>
-      <img src={BurgerLogo} alt="MyBurger" />
+      <img src={BurgerLogo} alt="MyBurger" style={{ height: props.height }} />
     </div>
   );
 };
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import classes from "./SideDrawer.module.css";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+const SideDrawer = (props) => {
+  return (
+    <div className={classes.SideDrawer}>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default SideDrawer;
